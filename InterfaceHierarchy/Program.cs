5f59fc3bc8935d98ce56a9a2ee9775bc9938ad06
Sergey Misko <sergik.m@outlook.com>
2022-06-28 00:02:10 +0500

    change the code in the Main method

--- InterfaceHierarchy/Program.cs
+++ InterfaceHierarchy/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using InterfaceHierarchy.Interfaces;
+using InterfaceHierarchy.Classes;
 
 namespace InterfaceHierarchy
 {
@@ -6,7 +8,15 @@ namespace InterfaceHierarchy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Simple Interface Hierarchy *****\n");
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpsideDown();
+
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
         }
     }
 }
