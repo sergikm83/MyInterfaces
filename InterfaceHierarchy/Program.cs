6fd12d6b6f5b983a2dd76b5213ff29aff02426d5
Sergey Misko <sergik.m@outlook.com>
2022-06-27 23:49:58 +0500

    add new project InterfaceHierarchy

---
+++ InterfaceHierarchy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
