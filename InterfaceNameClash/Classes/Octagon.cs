e9ce73f3dba7b6f3f22535d762f4016e03855b18
Sergey Misko <sergik.m@outlook.com>
2022-06-27 23:40:31 +0500

    add class Octagon

---
+++ InterfaceNameClash/Classes/Octagon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceNameClash.Interfaces;
+
+namespace InterfaceNameClash.Classes
+{
+    public class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        public void Draw() => Console.WriteLine("Drawing the Octagon...");
+    }
+}
