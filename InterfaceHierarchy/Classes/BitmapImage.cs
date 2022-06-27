339b1e8e852dadb42c6d11158d4f543a207adb50
Sergey Misko <sergik.m@outlook.com>
2022-06-27 23:56:26 +0500

    add class BitmapImage

---
+++ InterfaceHierarchy/Classes/BitmapImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceHierarchy.Interfaces;
+
+namespace InterfaceHierarchy.Classes
+{
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw() => Console.WriteLine("Drawing...");
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+            => Console.WriteLine("Drawing in a box...");
+        public void DrawUpsideDown() => Console.WriteLine("Drawing upside down!");
+    }
+}
