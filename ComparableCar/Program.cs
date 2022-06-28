541501363fb402ab3b2c5c052bf4a6b3aed39338
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 17:31:27 +0400

    изменил код в Main

--- ComparableCar/Program.cs
+++ ComparableCar/Program.cs
@@ -15,6 +15,16 @@ namespace ComparableCar
             myAutos[2] = new Car("Viper", 40, 34);
             myAutos[3] = new Car("Mel", 40, 4);
             myAutos[4] = new Car("Chucky", 40, 5);
+            // Отобразить текущее содержимое массива
+            Console.WriteLine("Here is the unordered set of cars:");
+            foreach(Car c in myAutos)
+                Console.WriteLine($"{c.CarID} {c.PetName}");
+            // Теперь отсортировать массив с применением IComparable!
+            Array.Sort(myAutos);
+            Console.WriteLine();
+            // Отобразить отсортированное содержимое массива
+            foreach (Car c in myAutos)
+                Console.WriteLine($"{c.CarID} {c.PetName}");
         }
     }
 }
