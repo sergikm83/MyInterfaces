39fac9000d57702083cd71ee41cb71952cf674b0
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 16:21:40 +0400

    add code to the Main method

--- CloneablePoint/Program.cs
+++ CloneablePoint/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CloneablePoint.Classes;
 
 namespace CloneablePoint
 {
@@ -6,7 +7,13 @@ namespace CloneablePoint
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Object Cloning *****\n");
+            // Две ссылки на один и тот же объект
+            Point p1 = new Point(xPos: 50, yPos: 50);
+            Point p2 = p1;
+            p2.X = 0;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
