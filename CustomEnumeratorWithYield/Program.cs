e6c82ed5c1306e2b6fec71ccf9bda0d348025374
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 16:00:13 +0400

    add code in the Main method

--- CustomEnumeratorWithYield/Program.cs
+++ CustomEnumeratorWithYield/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using CustomEnumeratorWithYield.Classes;
 
 namespace CustomEnumeratorWithYield
 {
@@ -6,7 +8,19 @@ namespace CustomEnumeratorWithYield
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with the Yield Keyword *****\n");
+            Garage carLot = new Garage();
+
+            // Получить элементы, используя GetEnumerator().
+            foreach(Car c in carLot)
+                Console.WriteLine($"{c.PetName} is going {c.CurrentSpeed} MPH");
+
+            Console.WriteLine();
+
+            // Получить элементы (в обратном порядке
+            // с применением bvtyjdfyyjuj bnthfnjhf
+            foreach(Car c in carLot.GetTheCars(returnReversed: true))
+                Console.WriteLine($"{c.PetName} is going {c.CurrentSpeed} MPH");
         }
     }
 }
