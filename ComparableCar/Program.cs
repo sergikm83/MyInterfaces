3f35b3111875c216484fcac4002659cf22201ae4
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 16:56:25 +0400

    накидал кода в Main

--- ComparableCar/Program.cs
+++ ComparableCar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ComparableCar.Classes;
 
 namespace ComparableCar
 {
@@ -6,7 +7,14 @@ namespace ComparableCar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Object Sorting *****\n");
+            // Создать массив объектов Car.
+            Car[] myAutos = new Car[5];
+            myAutos[0] = new Car("Rusty", 80, 1);
+            myAutos[1] = new Car("Mary", 40, 234);
+            myAutos[2] = new Car("Viper", 40, 34);
+            myAutos[3] = new Car("Mel", 40, 4);
+            myAutos[4] = new Car("Chucky", 40, 5);
         }
     }
 }
