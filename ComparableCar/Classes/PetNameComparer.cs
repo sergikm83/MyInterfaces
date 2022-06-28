d40af05d46483e5f0e5b025d2308ae79accea671
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 17:37:44 +0400

    добавил новый класс компаратор PetNameComparer

---
+++ ComparableCar/Classes/PetNameComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ComparableCar.Classes
+{
+    public class PetNameComparer : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Car t1 = o1 as Car;
+            Car t2 = o2 as Car;
+            if (t1 != null && t2 != null)
+                return String.Compare(t1.PetName, t2.PetName);
+            else
+                throw new ArgumentException("Parameters is not a Car!");
+        }
+    }
+}
