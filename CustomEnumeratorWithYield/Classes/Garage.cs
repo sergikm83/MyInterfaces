6830a30d4041061d69ee8f97b1a922560586bea9
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 15:40:27 +0400

    change namespace for added classes

--- CustomEnumeratorWithYield/Classes/Garage.cs
+++ CustomEnumeratorWithYield/Classes/Garage.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomEnumerator.Classes
+namespace CustomEnumeratorWithYield.Classes
 {
     public class Garage : IEnumerable
     {
--- CustomEnumeratorWithYield/Classes/Radio.cs
+++ CustomEnumeratorWithYield/Classes/Radio.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomEnumerator
+namespace CustomEnumeratorWithYield.Classes
 {
     public class Radio
     {
