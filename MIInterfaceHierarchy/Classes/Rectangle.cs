4b1cdde88cb83557e73487d8ab51dafda1ec3208
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 14:19:33 +0400

    add class Rectangle

---
+++ MIInterfaceHierarchy/Classes/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIInterfaceHierarchy.Interfaces;
+
+namespace MIInterfaceHierarchy.Classes
+{
+    public class Rectangle : IShape
+    {
+        public int GetNumberOfSides() => 4;
+        public void Draw() => Console.WriteLine("Drawing...");
+        public void Print() => Console.WriteLine("Printing...");
+    }
+}
