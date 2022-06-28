edd9ddf7674e7582d673fd83d473b2d771a459bf
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 17:46:27 +0400

    добавил компаратор по CurrentSpeed

---
+++ ComparableCar/Classes/CurrentSpeedComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ComparableCar.Classes
+{
+    public class CurrentSpeedComparer : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Car t1 = x as Car;
+            Car t2 = y as Car;
+            if (t1 != null && t2 != null)
+                return t1.CurrentSpeed.CompareTo(t2.CurrentSpeed);
+            else
+                throw new ArgumentException("Parameters is not a Car!");
+        }
+    }
+}
