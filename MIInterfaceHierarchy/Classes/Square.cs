48de91dbb64c377096f860f9085d80db2fb2dc2c
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 14:22:43 +0400

    add class Square

---
+++ MIInterfaceHierarchy/Classes/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIInterfaceHierarchy.Interfaces;
+
+namespace MIInterfaceHierarchy.Classes
+{
+    public class Square : IShape
+    {
+        public int GetNumberOfSides() => 4;
+        void IDrawable.Draw() => Console.WriteLine("Driwing square...");
+        void IPrintable.Draw() => Console.WriteLine("Drawing square before print.");
+        public void Print() => Console.WriteLine("Printing square...");
+    }
+}
