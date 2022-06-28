5358d355c1014d792d0ed21adbbfdcf9d7faf928
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 14:32:39 +0400

    add class Garage

---
+++ CustomEnumerator/Classes/Garage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator.Classes
+{
+    public class Garage
+    {
+        private Car[] carArray = new Car[4];
+        public Garage()
+        {
+            carArray[0] = new Car("Rusty", 30);
+            carArray[1] = new Car("Clunker", 55);
+            carArray[2] = new Car("Zippy", 30);
+            carArray[3] = new Car("Fred", 30);
+        }
+    }
+}
