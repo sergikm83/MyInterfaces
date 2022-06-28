00159b685ae6221ac42e904de70f94459a11879a
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 16:18:49 +0400

    add class Point

---
+++ CloneablePoint/Classes/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint.Classes
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point() { }
+
+        public override string ToString() => $"X = {X}; Y = {Y}";
+    }
+}
