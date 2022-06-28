508fda95ad6f45a34843705c6f62df261b9eba25
Sergey Misko <misko_si@akron-holding.ru>
2022-06-28 16:39:43 +0400

    изменил Main

--- CloneablePoint/Program.cs
+++ CloneablePoint/Program.cs
@@ -8,18 +8,21 @@ namespace CloneablePoint
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Object Cloning *****\n");
-            // Две ссылки на один и тот же объект
-            Point p1 = new Point(xPos: 50, yPos: 50);
-            Point p2 = p1;
-            p2.X = 0;
-            Console.WriteLine(p1);
-            Console.WriteLine(p2);
+            Console.WriteLine("Cloned p3 and stored new Point in p4");
 
-            Point p3 = new Point(xPos: 100, yPos: 100);
+            Point p3 = new Point(100, 100, "Jane");
             Point p4 = (Point)p3.Clone();
-            p4.X = 0;
-            Console.WriteLine(p3);
-            Console.WriteLine(p4);
+            Console.WriteLine("Before modification:");
+            Console.WriteLine($"p3: {p3}");
+            Console.WriteLine($"p4: {p4}");
+            p4.desc.PetName = "My new Point";
+            p4.X = 9;
+
+            Console.WriteLine("\nChanged p4.desc.PetName and p4.X");
+            Console.WriteLine("After modofication:");
+            Console.WriteLine($"p3: {p3}");
+            Console.WriteLine($"p4: {p4}");
+
         }
     }
 }
